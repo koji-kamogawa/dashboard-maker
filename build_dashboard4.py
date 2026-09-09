be69b59e5500f6f4a566e90df3a1a7a7aec21c2d
koji-kamogawa <koji.kamogawa.na@hitachi.com>
2026-09-09 17:36:22 +0900

    Update build_dashboard4.py
    Fixed font issue

--- build_dashboard4.py
+++ build_dashboard4.py
@@ -56,7 +56,7 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.font_manager as fm
-import japanize_matplotlib
+import matplotlib_fontja
 
 # ----------------------------------------------------------------------------
 # 日本語フォントの自動選択（無ければ警告を出して既定フォントで続行）
